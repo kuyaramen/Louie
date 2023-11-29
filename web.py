6a139aae8e6d003be33d9d67db22a265b445a894
kuyaramen <152365407+kuyaramen@users.noreply.github.com>
2023-11-29 14:21:18 +0800

    Create web.py
    I am creating an app

---
+++ web.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My webpage", page_icon=":tada",layout="wide")
+
+# ---- HEADER SECTION ----
+
+st.subheader("Hi, I am louie :face_in_clouds:")
+st.title("A student of SURIGAO DEL NORTE STATE UNIVERSITY")
+st.write(" I am willing to learn programming")
+
+
+
+
+
